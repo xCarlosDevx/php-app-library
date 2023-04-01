68b6fc30d7b11b0d3a18d6c81928175123c8b235
Carlos Alejandro La Hoz <78973956+xCarlosDevx@users.noreply.github.com>
2023-04-01 09:54:04 -0400

    Update conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -2,7 +2,7 @@
 function con()
 {
     try { 
-        return new PDO('mysql:host=localhost;dbname=libreria', 'root', '');
+        return new PDO('mysql:host=bpcirl5crjdpmmxkqy3f-mysql.services.clever-cloud.com;dbname=bpcirl5crjdpmmxkqy3f', 'ucrbaoq2wkjgkdf0', 'M8IaWOST3CHqz0BeYzsS');
     } catch (PDOException $e) {
         echo 'Error en la conexion: ' . $e->getMessage();
     }
